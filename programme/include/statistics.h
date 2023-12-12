ff5c64b3a7b17829234df4bdf57c57ff7a44f08e
mplanchot <mael.planchot@insa-rouen.fr>
2023-12-12 10:23:18 +0100

    stat .h

--- programme/include/statistics.h
+++ programme/include/statistics.h
@@ -1,4 +1,20 @@
 #ifndef STATISTICS_H_DEFINED
 #define STATISTICS_H_DEFINED
 
+#define MAX 256
+
+typedef struct
+{
+    int lesElements[MAX]; // type huffmantree
+    unsigned int nbOccurrence[MAX];
+} Statistics;
+
+Statistics statistics();
+
+unsigned int getCount(Statistics stat, int e);
+
+Statistics setCount(Statistics stat, int e, unsigned int n);
+
+Statistics increaseCount(Statistics stat, int e);
+
 #endif // STATISTICS_H_DEFINED
