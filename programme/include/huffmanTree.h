cea32d196c7633bccd68e2ca350ed95dd74df139
Alix ANNERAUD <alix@anneraud.fr>
2023-12-15 01:18:41 +0100

    Work on huffman tree header

--- programme/include/huffmanTree.h
+++ programme/include/huffmanTree.h
@@ -1,4 +1,24 @@
 #ifndef HUFFMAN_TREE_H_DEFINED
 #define HUFFMAN_TREE_H_DEFINED
 
+#include "common.h"
+
+typedef struct {
+// TODO : A compléter
+} HuffmanTree;
+
+HuffmanTree HuffmanTree_new_leaf(char value, unsigned int weight);
+
+HuffmanTree HuffmanTree_new_tree(HuffmanTree *left, HuffmanTree *right);
+
+bool HuffmanTree_is_leaf(const HuffmanTree *tree);
+
+char HuffmanTree_get_value(const HuffmanTree *tree);
+
+unsigned int HuffmanTree_get_weight(const HuffmanTree *tree);
+
+HuffmanTree* HuffmanTree_get_left_child(const HuffmanTree *tree);
+
+HuffmanTree* HuffmanTree_get_right_child(const HuffmanTree *tree);
+
 #endif // HUFFMAN_TREE_H_DEFINED
