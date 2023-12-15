8edbfce4c5dc3b00254ec0fa1ccf07f8e0387553
Alix ANNERAUD <alix@anneraud.fr>
2023-12-15 10:56:18 +0100

    Separate main from lib

---
+++ programme/lib/src/huffmanTree.c
@@ -0,0 +1,40 @@
+#include "huffmanTree.h"
+
+HuffmanTree* HuffmanTree_new_leaf(char value, unsigned int weight) {
+    // TODO : A compléter
+    return NULL;
+}
+
+HuffmanTree* HuffmanTree_new_tree(HuffmanTree *left, HuffmanTree *right) {
+    // TODO : A compléter
+    return NULL;
+}
+
+void HuffmanTree_delete(HuffmanTree *tree) {
+    // TODO : A compléter
+}
+
+bool HuffmanTree_is_leaf(const HuffmanTree *tree) {
+    // TODO : A compléter
+    return false;
+}
+
+char HuffmanTree_get_value(const HuffmanTree *tree) {
+    // TODO : A compléter
+    return '\0';
+}
+
+unsigned int HuffmanTree_get_weight(const HuffmanTree *tree) {
+    // TODO : A compléter
+    return 0;
+}
+
+HuffmanTree* HuffmanTree_get_left_child(const HuffmanTree *tree) {
+    // TODO : A compléter
+    return NULL;
+}
+
+HuffmanTree* HuffmanTree_get_right_child(const HuffmanTree *tree) {
+    // TODO : A compléter
+    return NULL;
+}
--- programme/src/main.c
+++ programme/src/main.c
@@ -1,6 +1,8 @@
 #include "stdio.h"
 
+#include "huffmanTree.h"
 
 int main() {
     printf("Hello world !");
+    HuffmanTree* tree = HuffmanTree_new_leaf('a', 1);
 }
