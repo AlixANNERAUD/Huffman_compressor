5f9f8d81fe97a725cdca1c27656ec28c0337d875
Alix ANNERAUD <alix@anneraud.fr>
2023-12-15 01:08:19 +0100

    Work on priority queue #34 #47

--- programme/include/priorityQueue.h
+++ programme/include/priorityQueue.h
@@ -1,4 +1,37 @@
 #ifndef PIRIORITY_QUEUE_H_DEFINED
 #define PIRIORITY_QUEUE_H_DEFINED
 
+#include "common.h"
+
+#define PRIORITY_QUEUE_MAXIMUM_SIZE 256
+
+/// @brief Structure représentant une file de priorité.
+typedef struct {
+    /// @brief Noeuds de la file de priorité.
+    int nodes[PRIORITY_QUEUE_MAXIMUM_SIZE];
+    /// @brief Taille de la file de priorité.
+    size_t length;
+} PriorityQueue;
+
+/// @brief Constructeur de file de priorité.
+/// @return File de priorité vide.
+PriorityQueue PriorityQueue_create();
+
+/// @brief Retourne la taille de la file de priorité.
+/// @param queue File de priorité
+/// @return Taille de la file de priorité.
+size_t PriorityQueue_get_length(const PriorityQueue *queue);
+
+/// @brief Défile le dernier élément de la file de priorité.
+/// @param queue File de priorité à défiler.
+/// @return Dernier élément de la file de priorité.
+int PriorityQueue_pop(PriorityQueue *queue);
+
+/// @brief Enfile un élément dans la file de priorité.
+/// @param queue File de priorité.
+/// @param node Nœud à enfiler.
+void PriorityQueue_push(PriorityQueue *queue, int node);
+
+
+
 #endif // PIRIORITY_QUEUE_H_DEFINED
---
+++ programme/src/priorityQueue.c
@@ -0,0 +1,40 @@
+#include "priorityQueue.h"
+
+PriorityQueue PriorityQueue_create() {
+    PriorityQueue priorityQueue;
+    priorityQueue.length = 0;
+    return priorityQueue;
+}
+
+size_t PriorityQueue_length(const PriorityQueue *priorityQueue) {
+    // Préconditions
+    assert(priorityQueue != NULL);
+
+    return priorityQueue->length;
+}
+
+int PriorityQueue_pop(PriorityQueue *priorityQueue) {
+    // Préconditions
+    assert(priorityQueue != NULL);
+    assert(priorityQueue->length > 0);
+
+    int node = priorityQueue->nodes[priorityQueue->length - 1];
+    priorityQueue->length--;
+    return node;
+}
+
+void PriorityQueue_push(PriorityQueue *priorityQueue, int node) {
+    // Préconditions
+    assert(priorityQueue != NULL);
+    assert(priorityQueue->length < PRIORITY_QUEUE_MAXIMUM_SIZE);
+
+    size_t i = priorityQueue->length;
+    // On décale les éléments de la file pour insérer le nouvel élément
+    // TODO : à revoir pour adapter au type arbre d'Huffman
+    while (priorityQueue->nodes[i - 1] > node && i > 0) {
+        priorityQueue->nodes[i] = priorityQueue->nodes[i - 1];
+        i--;
+    }
+    
+    priorityQueue->length++;
+}
