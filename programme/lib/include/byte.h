040a43b4f354d6cb92c99cd87b65f63aea238a18
fzhou <fangxuan.zhou@insa-rouen.fr>
2023-12-16 23:07:38 +0100

    I tried to do the .h for Byte

--- programme/lib/include/byte.h
+++ programme/lib/include/byte.h
@@ -1,1 +1,11 @@
 #pragma once
+
+typedef Bit Byte[8];
+
+Byte byte_byte(unsigned int natural);
+
+unsigned int byte_byte_to_natural(Byte byte);
+
+void byte_set_bit(Byte* byte, unsigned int position, Bit bit);
+
+Bit byte_get_bit(Byte byte, unsigned int position);
