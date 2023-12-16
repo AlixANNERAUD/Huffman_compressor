8eb3931b543839f2c57296b21240aa6151d9fe74
Alix ANNERAUD <alix@anneraud.fr>
2023-12-16 17:15:33 +0100

    Work on decompress #32

--- programme/include/decompress.h
+++ programme/include/decompress.h
@@ -1,18 +1,30 @@
 #ifndef DECOMPRESS_H_INCLUDED
 #define DECOMPRESS_H_INCLUDED
 
+// - Bibliothèques 
+
 #include "common.h"
 #include <stdio.h>
 
+// - Déclarations
+
+// - - Types
+
 typedef enum {
-    DECOMPRESS_ERROR_OK,
-    DECOMPRESS_FILE_ERROR,
-    DECOMPRESS_ERROR_INVALID_HEADER,
+    DECOMPRESS_RESULT_OK,
+    DECOMPRESS_ERROR_FILE,
+    DECOMPRESS_ERROR_PREMATURE_END_OF_FILE,
+    DECOMPRESS_ERROR_INVALID_HEADER
+} DecompressResult;
 
-} DecompressError;
+// - - Fonctions
 
-bool decompress(FILE* input, FILE* output);
+/// @brief Fonction principale pour décompresser un fichier compresser par compress.
+/// @param input Fichier compressé
+/// @param output Fichier décompressé
+/// @return Résultat de la décompression
+DecompressResult decompress(FILE* input, FILE* output);
 
-void decompress_error_to_string(DecompressError error, char* buffer, size_t buffer_size);
+void decompress_error_to_string(DecompressResult error, char* buffer, size_t buffer_size);
 
 #endif // DECOMPRESS_H_INCLUDED
--- programme/src/decompress.c
+++ programme/src/decompress.c
@@ -1,53 +1,103 @@
 #include "decompress.h"
 
+#include "huffmanTree.h"
+#include "statistics.h"
+
 #include <string.h>
 
-bool is_huffman_compressed_file(FILE* input) {
+// - Définitions
+
+// - - Fonctions privées
+
+bool is_huffman_compressed_file(FILE *input)
+{
     char identifier[4];
-    if (fread(identifier, sizeof(char), sizeof(identifier), input) != 4) {
+    if (fread(identifier, sizeof(char), sizeof(identifier), input) != 4)
         return false;
-    }
 
-    if (strncmp(identifier, "HUFF", sizeof(identifier)) != 0) {
+    if (strncmp(identifier, "HUFF", sizeof(identifier)) != 0)
         return false;
-    }
-
 }
 
-DecompressError read_header(FILE* input, size_t* size) {
-    if (!is_huffman_compressed_file(input)) {
+DecompressResult read_header(FILE *input, Statistics *statistics, size_t *size)
+{
+    // - Lecture de l'en-tête
+    // - - Vérification de l'identifiant
+    if (!is_huffman_compressed_file(input))
         return DECOMPRESS_ERROR_INVALID_HEADER;
-    }
-
-    if (fread(size, sizeof(size_t), 1, input) != 1) {
-        return DECOMPRESS_FILE_ERROR;
-    }
-
-    for (size_t i = 0; i < 256; ++i) {
-        size_t frequency;
-        if (fread(&frequency, sizeof(size_t), 1, input) != 1) {
-            return DECOMPRESS_FILE_ERROR;
-        }
-    }
-
+    // - - Lecture de la taille des données compressées
+    if (fread(size, sizeof(size_t), 1, input) != 1)
+        return DECOMPRESS_ERROR_INVALID_HEADER;
+    // - - Lecture des statistiques
+    unsigned char buffer[sizeof(statistics->nbOccurrence)];
+    if (fread(buffer, sizeof(unsigned char), sizeof(buffer), input) != sizeof(buffer))
+        return DECOMPRESS_ERROR_INVALID_HEADER;   
+    statistics_deserialize(statistics, buffer); // On désérialise les statistiques
 }
 
-DecompressError decompress(FILE *input, FILE *output) {
+DecompressResult decompress_data(FILE* input, FILE* output, const HuffmanTree* tree, size_t size) {
+    while (feof(input) == 0) {
+       // TODO : Implémenter la fonction
 
 
+    }
 }
 
-DecompressError decompress_error_to_string(DecompressError error, char *buffer, size_t buffer_size) {
-    switch (error) {
-        case DECOMPRESS_ERROR_OK:
-            strncpy(buffer, "No error", buffer_size);
-            break;
-        case DECOMPRESS_ERROR_INVALID_HEADER:
-            strncpy(buffer, "Invalid header", buffer_size);
-            break;
-        default:
-            strncpy(buffer, "Unknown error", buffer_size);
-            break;
+// - - Fonctions publiques
+
+DecompressResult decompress(FILE *input, FILE *output)
+{
+    // Préconditions
+    assert(input != NULL);
+    assert(output != NULL);
+
+    if (ferror(input) || ferror(output))
+        return DECOMPRESS_ERROR_FILE;
+
+    if (feof(input))
+        return DECOMPRESS_ERROR_PREMATURE_END_OF_FILE;
+
+    DecompressResult result = DECOMPRESS_RESULT_OK;
+    size_t size;
+    Statistics statistics = statistics_create();
+    result = read_header(input, &statistics, &size);
+
+    if (result != DECOMPRESS_RESULT_OK)
+        return result;
+
+    HuffmanTree *tree = build_huffman_tree(&statistics);
+
+    assert(tree != NULL);
+
+    if (feof(input) != 0)
+        return DECOMPRESS_ERROR_PREMATURE_END_OF_FILE;
+
+    result = decompress_data(input, output, tree, size);
+
+    huffman_tree_delete(tree);
+ 
+    return result;
+}
+
+void decompress_error_to_string(DecompressResult error, char *buffer, size_t buffer_size)
+{
+    switch (error)
+    {
+    case DECOMPRESS_RESULT_OK:
+        strncpy(buffer, "No error", buffer_size);
+        break;
+    case DECOMPRESS_ERROR_FILE:
+        strncpy(buffer, "File error", buffer_size);
+        break;
+    case DECOMPRESS_ERROR_PREMATURE_END_OF_FILE:
+        strncpy(buffer, "Premature end of file", buffer_size);
+        break;
+    case DECOMPRESS_ERROR_INVALID_HEADER:
+        strncpy(buffer, "Invalid header", buffer_size);
+        break;
+    default:
+        strncpy(buffer, "Unknown error", buffer_size);
+        break;
     }
     return error;
 }
