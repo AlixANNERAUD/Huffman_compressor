e47b1a218f8e002b2def569f03fe0d85578f7b85
Alix ANNERAUD <alix@anneraud.fr>
2023-12-16 17:09:25 +0100

    Write skeleton of binary code #36

--- programme/lib/include/binaryCode.h
+++ programme/lib/include/binaryCode.h
@@ -1,4 +1,28 @@
 #ifndef BINARY_CODE_H_DEFINED
 #define BINARY_CODE_H_DEFINED
 
+#include "common.h"
+
+#include "bit.h"
+
+typedef struct {
+    // TODO : A compléter
+} BinaryCode;
+
+BinaryCode binary_code_create();
+
+void binary_code_delete(BinaryCode* binaryCode);
+
+size_t binary_code_get_length(const BinaryCode* binaryCode);
+
+Bit binary_code_get_bit(const BinaryCode* binaryCode, size_t index);
+
+void binary_code_add_bit(BinaryCode* binaryCode, Bit bit);
+
+void binary_code_remove_bit(BinaryCode* binaryCode, size_t index);
+
+void binary_code_remove_last_bit(BinaryCode* binaryCode);
+
+BinaryCode binary_code_clone(const BinaryCode* binaryCode);
+
 #endif // BINARY_CODE_H_DEFINED
---
+++ programme/lib/src/binaryCode.c
@@ -0,0 +1,36 @@
+#include "binaryCode.h"
+
+#include <assert.h>
+
+BinaryCode binary_code_create() {
+    // TODO : Implémenter la fonction
+}
+
+void binary_code_delete(BinaryCode* code) {
+    // TODO : Implémenter la fonction
+}
+
+size_t binary_code_get_length(const BinaryCode* code) {
+    // TODO : Implémenter la fonction
+}
+
+Bit binary_code_get_bit(const BinaryCode* code, size_t index) {
+    // TODO : Implémenter la fonction
+}
+
+void binary_code_add_bit(BinaryCode* code, Bit bit) {
+    // TODO : Implémenter la fonction
+}
+
+void binary_code_remove_bit(BinaryCode* code, size_t index) {
+    // TODO : Implémenter la fonction
+}
+
+void binary_code_remove_last_bit(BinaryCode* code) {
+    // TODO : Implémenter la fonction
+}
+
+BinaryCode binary_code_clone(const BinaryCode* code) {
+    // TODO : Implémenter la fonction
+}
+
