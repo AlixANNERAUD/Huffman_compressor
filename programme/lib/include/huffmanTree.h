1ffa200ee3a645b757f79c85301c9249a925344c
Alix ANNERAUD <alix@anneraud.fr>
2023-12-16 16:40:26 +0100

    Use proper casing for huffman tree #33

--- programme/lib/include/huffmanTree.h
+++ programme/lib/include/huffmanTree.h
@@ -7,20 +7,20 @@ typedef struct {
 // TODO : A compléter
 } HuffmanTree;
 
-HuffmanTree* HuffmanTree_new_leaf(char value, unsigned int weight);
+HuffmanTree* huffman_tree_new_leaf(char value, unsigned int weight);
 
-HuffmanTree* HuffmanTree_new_tree(HuffmanTree *left, HuffmanTree *right);
+HuffmanTree* huffman_tree_new_tree(HuffmanTree *left, HuffmanTree *right);
 
-void HuffmanTree_delete(HuffmanTree *tree);
+void huffman_tree_delete(HuffmanTree *tree);
 
-bool HuffmanTree_is_leaf(const HuffmanTree *tree);
+bool huffman_tree_is_leaf(const HuffmanTree *tree);
 
-char HuffmanTree_get_value(const HuffmanTree *tree);
+char huffman_tree_get_value(const HuffmanTree *tree);
 
-unsigned int HuffmanTree_get_weight(const HuffmanTree *tree);
+unsigned int huffman_tree_get_weight(const HuffmanTree *tree);
 
-HuffmanTree* HuffmanTree_get_left_child(const HuffmanTree *tree);
+HuffmanTree* huffman_tree_get_left_child(const HuffmanTree *tree);
 
-HuffmanTree* HuffmanTree_get_right_child(const HuffmanTree *tree);
+HuffmanTree* huffman_tree_get_right_child(const HuffmanTree *tree);
 
 #endif // HUFFMAN_TREE_H_DEFINED
--- programme/tests/src/huffmanTree.c
+++ programme/tests/src/huffmanTree.c
@@ -1,43 +1,43 @@
 #include "huffmanTree.h"
 #include <CUnit/Basic.h>
 
-void test_HuffmanTree_new_leaf()
+void test_huffman_tree_new_leaf()
 {
-    HuffmanTree *tree = HuffmanTree_new_leaf('a', 6);
+    HuffmanTree *tree = huffman_tree_new_leaf('a', 6);
     CU_ASSERT_PTR_NOT_NULL(tree);
-    CU_ASSERT_EQUAL(HuffmanTree_get_value(tree), 'a');
-    CU_ASSERT_EQUAL(HuffmanTree_get_weight(tree), 6);
-    HuffmanTree_delete(tree);
+    CU_ASSERT_EQUAL(huffman_tree_get_value(tree), 'a');
+    CU_ASSERT_EQUAL(huffman_tree_get_weight(tree), 6);
+    huffman_tree_delete(tree);
 }
 
-void test_HuffmanTree_new_tree()
+void test_huffman_tree_new_tree()
 {
-    HuffmanTree *left = HuffmanTree_new_leaf('a', 1);
-    HuffmanTree *right = HuffmanTree_new_leaf('b', 2);
-    HuffmanTree *tree = HuffmanTree_new_tree(left, right);
+    HuffmanTree *left = huffman_tree_new_leaf('a', 1);
+    HuffmanTree *right = huffman_tree_new_leaf('b', 2);
+    HuffmanTree *tree = huffman_tree_new_tree(left, right);
     CU_ASSERT_PTR_NOT_NULL(tree);
-    CU_ASSERT_PTR_NOT_NULL(HuffmanTree_get_left_child(tree));
-    CU_ASSERT_PTR_NOT_NULL(HuffmanTree_get_right_child(tree));
-    CU_ASSERT_PTR_EQUAL(HuffmanTree_get_left_child(tree), left);
-    CU_ASSERT_PTR_EQUAL(HuffmanTree_get_right_child(tree), right);
-    HuffmanTree_delete(tree);
+    CU_ASSERT_PTR_NOT_NULL(huffman_tree_get_left_child(tree));
+    CU_ASSERT_PTR_NOT_NULL(huffman_tree_get_right_child(tree));
+    CU_ASSERT_PTR_EQUAL(huffman_tree_get_left_child(tree), left);
+    CU_ASSERT_PTR_EQUAL(huffman_tree_get_right_child(tree), right);
+    huffman_tree_delete(tree);
 }
 
-void test_HuffmanTree_is_leaf()
+void test_huffman_tree_is_leaf()
 {
-    HuffmanTree *left = HuffmanTree_new_leaf('a', 1);
-    CU_ASSERT_TRUE(HuffmanTree_is_leaf(left));
-    HuffmanTree *right = HuffmanTree_new_leaf('b', 2);
-    CU_ASSERT_TRUE(HuffmanTree_is_leaf(right));
-    HuffmanTree *tree = HuffmanTree_new_tree(left, right);
-    CU_ASSERT_FALSE(HuffmanTree_is_leaf(tree));
-    HuffmanTree_delete(tree);
+    HuffmanTree *left = huffman_tree_new_leaf('a', 1);
+    CU_ASSERT_TRUE(huffman_tree_is_leaf(left));
+    HuffmanTree *right = huffman_tree_new_leaf('b', 2);
+    CU_ASSERT_TRUE(huffman_tree_is_leaf(right));
+    HuffmanTree *tree = huffman_tree_new_tree(left, right);
+    CU_ASSERT_FALSE(huffman_tree_is_leaf(tree));
+    huffman_tree_delete(tree);
 }
 
-void HuffmanTree_add_tests()
+void huffman_tree_add_tests()
 {
     CU_pSuite suite = CU_add_suite("HuffmanTree", NULL, NULL);
-    CU_add_test(suite, "test_HuffmanTree_new_leaf", test_HuffmanTree_new_leaf);
-    CU_add_test(suite, "test_HuffmanTree_new_tree", test_HuffmanTree_new_tree);
-    CU_add_test(suite, "test_HuffmanTree_is_leaf", test_HuffmanTree_is_leaf);
+    CU_add_test(suite, "test_huffman_tree_new_leaf", test_huffman_tree_new_leaf);
+    CU_add_test(suite, "test_huffman_tree_new_tree", test_huffman_tree_new_tree);
+    CU_add_test(suite, "test_huffman_tree_is_leaf", test_huffman_tree_is_leaf);
 }
