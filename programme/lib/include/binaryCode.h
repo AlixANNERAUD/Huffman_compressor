8cbc636c2f8e59b4f8522b180f2d77b8e9ae7702
mplanchot <mael.planchot@insa-rouen.fr>
2023-12-18 21:07:04 +0100

    binaryCode.h rectify

--- programme/lib/include/binaryCode.h
+++ programme/lib/include/binaryCode.h
@@ -4,21 +4,24 @@
 
 #include "bit.h"
 
+#define BinaryCode_MAX 1000
+
 typedef struct {
-    // TODO : A compléter
+    Bit bits[BinaryCode_MAX]; 
+    unsigned int length;
 } BinaryCode;
 
 BinaryCode binary_code_create();
 
 void binary_code_delete(BinaryCode* binaryCode);
 
-size_t binary_code_get_length(const BinaryCode* binaryCode);
+unsigned int binary_code_get_length(const BinaryCode* binaryCode);
 
-Bit binary_code_get_bit(const BinaryCode* binaryCode, size_t index);
+Bit binary_code_get_bit(const BinaryCode* binaryCode, unsigned int index);
 
 void binary_code_add_bit(BinaryCode* binaryCode, Bit bit);
 
-void binary_code_remove_bit(BinaryCode* binaryCode, size_t index);
+void binary_code_remove_bit(BinaryCode* binaryCode, unsigned int index);
 
 void binary_code_remove_last_bit(BinaryCode* binaryCode);
 
