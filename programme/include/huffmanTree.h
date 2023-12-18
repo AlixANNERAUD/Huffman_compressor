e7a9c7068b7af4ba99b412d7e0f0ba93aeeda929
atranouez <alienor.tranouez@insa-rouen.fr>
2023-12-18 16:09:48 +0100

    V1 for huffmanTree implementation #33

--- programme/include/huffmanTree.h
+++ programme/include/huffmanTree.h
@@ -3,8 +3,58 @@
 // - Bibliothèques 
 
 #include "common.h"
+#include "byte.h"
 #include <stdio.h>
 
 // - Déclarations
 
 // - - Types
+
+typedef struct node {
+    byte value;
+    unsigned int weight;
+    struct node *leftChild;
+    struct node *rightChild;
+}
+
+typedef *node huffmanTree;
+
+// - - Fonctions
+
+
+/// @brief Fonction créant une feuille à partir de sa valeur et du poids associé
+/// @param value byte pour lequel on crée la feuille
+/// @param weight poids (nb d'occurence) du byte dans le fichier
+/// @return Un arbre d'Huffman qui est une feuille
+huffmanTree newLeaf(byte value, unsigned int weight);
+
+/// @brief Fonction créant un arbre à partir de ses fils gauche et droit
+/// @param tree1 Futur fils gauhe
+/// @param tree2 Futur fils droit
+/// @return Un arbre d'huffman de fils gauche tree1 et droit tree2, weight prend la somme de weight tree1 et tree2, et value une valeur arbitraire (byte (0))
+huffmanTree newTree(huffmanTree *tree1, huffmanTree *tree2);
+
+/// @brief Fonction permettant de savoir si un arbe est une feuille (ie si ses fils sont vides)
+/// @param tree 
+/// @return Vrai si ses deux fils sont vides, faux sinon
+bool isLeaf (huffmanTree *tree);
+
+/// @brief Accesseur de la valeur d'une feuille (Précondition : Doit être une feuille)
+/// @param tree 
+/// @return La valeur de la feuille
+byte getValue(huffmanTree *tree);
+
+/// @brief Accesseur du poids d'un noeud
+/// @param tree 
+/// @return La valeur du noeud racine
+unsigned int getWeight(huffmanTree *tree);
+
+/// @brief Accesseur du fils gauche d'un arbre (Précondition : Ne doit pas être une feuille)
+/// @param tree 
+/// @return Le fils gauche
+huffmanTree getLeftChild(huffmanTree *tree);
+
+/// @brief Accesseur du fils droit d'un arbre (Précondition : Ne doit pas être une feuille)
+/// @param tree 
+/// @return Le fils droit
+huffmanTree getRightChild(huffmanTree *tree);
--- programme/src/huffmanTree.c
+++ programme/src/huffmanTree.c
@@ -9,7 +9,60 @@
 // - - Fonctions privées
 
 
-
-
 // - - Fonctions publiques
 
+
+huffmanTree newLeaf(byte value, unsigned int weight)
+{
+    huffmanTree *tree = malloc(sizeof(node));
+
+    tree->value = value;
+    tree->weight = weight;
+    tree->*leftChild = NULL;
+    tree->*rightChild = NULL;
+
+    return tree;
+}
+
+huffmanTree newTree(huffmanTree *tree1, huffmanTree *tree2)
+{
+    huffmanTree *tree = newLeaf(byte(0), tree1->weight + tree2->weight);
+    tree->*leftChild = *tree1;
+    tree->*rightChild = *tree2;
+
+    return *tree;
+}
+
+bool isLeaf (huffmanTree *tree)
+{
+    return ((tree->*leftChild==NULL) && (tree->*rightChild==NULL));
+}
+
+byte getValue(huffmanTree *tree)
+{
+    //Précondition
+    assert(isLeaf(tree));
+
+    return tree->value;
+}
+
+unsigned int getWeight(huffmanTree *tree)
+{
+    return tree->weight;
+}
+
+huffmanTree getLeftChild(huffmanTree *tree)
+{
+    //Prédondition
+    assert(!(isLeaf(tree)));
+
+    return tree->*leftChild;
+}
+
+huffmanTree getRightChild(huffmanTree *tree)
+{
+    //Précondition
+    assert(!(isLeaf(tree)));
+
+    return tree->*rightChild;
+}
