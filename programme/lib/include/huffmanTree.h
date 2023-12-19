6152bf1ff8ae101dfa86102c4853a778ff1c4299
atranouez <alienor.tranouez@insa-rouen.fr>
2023-12-19 10:04:56 +0100

    Move ht and fix typo #33

--- programme/lib/include/huffmanTree.h
+++ programme/lib/include/huffmanTree.h
@@ -1,28 +1,72 @@
 #pragma once
 
+// - Bibliothèques 
+
 #include "common.h"
+#include "byte.h"
+#include <stdio.h>
 
+// - Déclarations
+
+// - - Types
+
+/// @brief 
 typedef struct {
-// TODO : A compléter
-} HuffmanTree;
+    /// @brief Valeur (byte) du noeud. NB : Utile uniquement dans une feuille. 
+    Byte value;
+    /// @brief Poids du noeud, son poids propre s'il est une feuille, la somme du poids de ses fils sinon. 
+    unsigned int weight;
+    /// @brief Fils gauche.
+    struct Node* leftChild;
+    /// @brief Fils droit.
+    struct Node* rightChild;
+} Node;
 
+typedef Node* HuffmanTree;
 
-HuffmanTree* huffman_tree_new_leaf(char value, unsigned int weight);
+// - - Fonctions
 
-HuffmanTree* huffman_tree_new_tree(HuffmanTree *left, HuffmanTree *right);
+/// @brief Fonction créant une feuille à partir de sa valeur et du poids associé
+/// @param value Byte pour lequel on crée la feuille
+/// @param weight poids (nb d'occurence) du Byte dans le fichier
+/// @return Un arbre d'Huffman qui est une feuille
+HuffmanTree huffman_tree_new_leaf(char value, unsigned int weight);
 
-void huffman_tree_delete(HuffmanTree *tree);
+/// @brief Fonction créant un arbre à partir de ses fils gauche et droit
+/// @param tree1 Futur fils gauhe
+/// @param tree2 Futur fils droit
+/// @return Un arbre d'huffman de fils gauche tree1 et droit tree2, weight prend la somme de weight tree1 et tree2, et value une valeur arbitraire (Byte (0))
+HuffmanTree huffman_tree_new_tree(HuffmanTree left, HuffmanTree right);
 
-bool huffman_tree_is_leaf(const HuffmanTree *tree);
+/// @brief Fonction permettant de savoir si un arbe est une feuille (ie si ses fils sont vides)
+/// @param tree 
+/// @return Vrai si ses deux fils sont vides, faux sinon
+bool huffman_tree_is_leaf(const HuffmanTree tree);
 
-char huffman_tree_get_value(const HuffmanTree *tree);
+/// @brief Accesseur de la valeur d'une feuille (Précondition : Doit être une feuille)
+/// @param tree 
+/// @return La valeur de la feuille
+char huffman_tree_get_value(const HuffmanTree tree);
 
-unsigned int huffman_tree_get_weight(const HuffmanTree *tree);
+/// @brief Accesseur du poids d'un noeud
+/// @param tree 
+/// @return La valeur du noeud racine
+unsigned int huffman_tree_get_weight(const HuffmanTree tree);
 
-HuffmanTree* huffman_tree_get_left_child(const HuffmanTree *tree);
+/// @brief Accesseur du fils gauche d'un arbre (Précondition : Ne doit pas être une feuille)
+/// @param tree 
+/// @return Le fils gauche
+HuffmanTree huffman_tree_get_left_child(const HuffmanTree tree);
 
-HuffmanTree* huffman_tree_get_right_child(const HuffmanTree *tree);
+/// @brief Accesseur du fils droit d'un arbre (Précondition : Ne doit pas être une feuille)
+/// @param tree 
+/// @return Le fils droit
+HuffmanTree huffman_tree_get_right_child(const HuffmanTree tree);
+
+void huffman_tree_delete(HuffmanTree tree);
 
 typedef struct Statistics Statistics; // Déclaration anticipée pour réduire les dépendances inter-fichiers.
+
+typedef struct PriorityQueue PriorityQueue; // De même 
 
 HuffmanTree* huffman_tree_from_statistic(const Statistics *statistics); // Transtypage
--- programme/lib/src/huffmanTree.c
+++ programme/lib/src/huffmanTree.c
@@ -2,46 +2,96 @@
 
 #include "statistics.h"
 
-HuffmanTree* huffman_tree_new_leaf(char value, unsigned int weight) {
-    // TODO : A compléter
-    return NULL;
+#include "huffmanTree.h"
+
+// - Définitions
+
+// - - Fonctions privées
+
+
+// - - Fonctions publiques
+
+HuffmanTree huffman_tree_new_leaf(char value, unsigned int weight) {
+
+    HuffmanTree tree = malloc(sizeof(Node));
+
+    tree->value = value;
+    tree->weight = weight;
+    tree->leftChild = NULL;
+    tree->rightChild = NULL;
+
+    return tree;
 }
 
-HuffmanTree* huffman_tree_new_tree(HuffmanTree *left, HuffmanTree *right) {
-    // TODO : A compléter
-    return NULL;
+HuffmanTree huffman_tree_new_tree(HuffmanTree left, HuffmanTree right) {
+    //Préconditions (vérifier non vides)
+    assert(left);
+    assert(right);
+
+    HuffmanTree tree = newLeaf(Byte(0), left->weight + right->weight);
+    tree->leftChild = left;
+    tree->rightChild = right;
+
+    return tree;
 }
 
-void huffman_tree_delete(HuffmanTree *tree) {
-    // TODO : A compléter
+void huffman_tree_delete(HuffmanTree tree) {
+    //Précondition
+    assert(!tree);
+
+    HuffmanTree tmpTree = tree;
+    
+    if (tmpTree->leftChild) huffman_tree_delete(&tmpTree->leftChild);
+
+    if (tmpTree->rightChild) huffman_tree_delete(&tmpTree->rightChild);
+
+    free(tmpTree);
+
+    tree = NULL;
 }
 
-bool huffman_tree_is_leaf(const HuffmanTree *tree) {
-    // TODO : A compléter
-    return false;
+bool huffman_tree_is_leaf(const HuffmanTree tree) {
+    //Précondtion
+    assert(tree);
+
+    return ((tree->leftChild==NULL) && (tree->rightChild==NULL));
 }
 
-char huffman_tree_get_value(const HuffmanTree *tree) {
-    // TODO : A compléter
-    return '\0';
+char huffman_tree_get_value(const HuffmanTree tree) {
+    //Préconditions
+    assert(tree);
+    assert(isLeaf(tree));
+
+    return tree->value;
 }
 
-unsigned int huffman_tree_get_weight(const HuffmanTree *tree) {
-    // TODO : A compléter
-    return 0;
+unsigned int huffman_tree_get_weight(const HuffmanTree tree) {
+    //Précondtion
+    assert(tree);
+
+    return tree->weight;
 }
 
-HuffmanTree* huffman_tree_get_left_child(const HuffmanTree *tree) {
-    // TODO : A compléter
-    return NULL;
+HuffmanTree huffman_tree_get_left_child(const HuffmanTree tree) {
+   //Prédonditions
+    assert(tree);
+    assert(!(isLeaf(tree)));
+
+    return tree->leftChild;
 }
 
-HuffmanTree* huffman_tree_get_right_child(const HuffmanTree *tree) {
-    // TODO : A compléter
-    return NULL;
+HuffmanTree huffman_tree_get_right_child(const HuffmanTree tree) {
+   //Préconditions
+    assert(tree);
+    assert(!(isLeaf(tree)));
+
+    return tree->rightChild;
 }
 
-HuffmanTree* huffman_tree_from_statistic(const Statistics *statistics) {
-    // TODO : A compléter
+HuffmanTree* huffman_tree_from_statistic(const Statistics* statistics) {
+    //Précondition
+    assert(statistics);
+    
+
     return NULL;
 }
