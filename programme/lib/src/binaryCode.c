7ec1d69eb1dc7eefab37210eca3518ee10584f91
mplanchot <mael.planchot@insa-rouen.fr>
2023-12-19 10:42:42 +0100

    fix binary code

--- programme/lib/src/binaryCode.c
+++ programme/lib/src/binaryCode.c
@@ -13,19 +13,19 @@ unsigned int binary_code_get_length(const BinaryCode* code) {
 }
 
 Bit binary_code_get_bit(const BinaryCode* code, unsigned int index) {
-    assert(index >= 0 && index <= binary_code_get_length(code));
+    assert(!(index >= 0 && index <= binary_code_get_length(code)));
     return code->bits[index];
 }
 
 void binary_code_add_bit(BinaryCode* code, Bit bit) {
-    assert(binary_code_get_length(code)< BinaryCode_MAX);
+    assert(!(binary_code_get_length(code)< BinaryCode_MAX));
     code->length = code->length+1;
     code->bits[code->length-1]=bit;
 }
 
 void binary_code_remove_bit(BinaryCode* code, unsigned int index) {
-    assert(index >= 0 && index < binary_code_get_length(code))
-    assert(binary_code_get_length(code) > 0);
+    assert(!(index >= 0 && index < binary_code_get_length(code)))
+    assert(!(binary_code_get_length(code) > 0));
     for(int i=index;i<binary_code_get_length(code)-1;i=i+1){
         code->bits[i]=code->bits[i+1];
     }
@@ -33,6 +33,6 @@ void binary_code_remove_bit(BinaryCode* code, unsigned int index) {
 }
 
 void binary_code_remove_last_bit(BinaryCode* code) {
-    assert(binary_code_get_length(code)> 0);
+    assert(!(binary_code_get_length(code)> 0));
     code->length = code->length -1;
 }
