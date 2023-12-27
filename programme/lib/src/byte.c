e1e9facf4544bd3e10e6f15189632805993c6bbd
fzhou <fangxuan.zhou@insa-rouen.fr>
2023-12-27 21:16:45 +0100

    I changed the layout of byte.c

--- programme/lib/src/byte.c
+++ programme/lib/src/byte.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 
 Byte byte_create(unsigned int natural){
+    // Préconditions
     assert(natural <= 0xFF);
 
     Byte res = natural;
@@ -17,21 +18,26 @@ unsigned int byte_to_natural(Byte byte){
 }
 
 void byte_set_bit(Byte* byte, unsigned int position, Bit bit) {
+    // Préconditions
     assert(position <= 7);
 
     Byte temp = byte;
     if (bit == BIT_0)
-        temp &= ~(1 << position); // &= AND, ~ NOT, décaler à gauche puis fixer le bit 0 à la position donnée
+        temp &= ~(1 << position); 
+    // &= AND, ~ NOT, décaler à gauche puis fixer le bit 0 à la position donnée
     else 
-        temp |= (1 << position); // |= OR, décaler à gauche puis fixer le bit 1 à la position donnée
+        temp |= (1 << position); 
+    // |= OR, décaler à gauche puis fixer le bit 1 à la position donnée
     *byte = temp;
 }
 
 Bit byte_get_bit(Byte byte, unsigned int position){
+    // Préconditions
     assert(position <= 7);
 
     Bit res;
-    if ((byte >> position)&1 == 1) // décaler à droite puis comparer avec 1 (AND) donne le bit à la position 
+    if ((byte >> position)&1 == 1) 
+    // décaler à droite puis comparer avec 1 (en utilisant AND) donne le bit à la position donnée
         res = BIT_1;
     else
         res = BIT_0;
