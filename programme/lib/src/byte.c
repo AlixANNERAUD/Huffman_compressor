31f36ec6d012888079d341b209f34e6aa59d67c0
Alix ANNERAUD <alix@anneraud.fr>
2023-12-29 00:13:51 +0100

    Add missing dereferencing operator in byte

--- programme/lib/src/byte.c
+++ programme/lib/src/byte.c
@@ -21,7 +21,7 @@ void byte_set_bit(Byte* byte, unsigned int position, Bit bit) {
     // Préconditions
     assert(position <= 7);
 
-    Byte temp = byte;
+    Byte temp = *byte;
     if (bit == BIT_0)
         temp &= ~(1 << position); 
     // &= AND, ~ NOT, décaler à gauche puis fixer le bit 0 à la position donnée
@@ -36,7 +36,7 @@ Bit byte_get_bit(Byte byte, unsigned int position){
     assert(position <= 7);
 
     Bit res;
-    if ((byte >> position)&1 == 1) 
+    if (((byte >> position)&1) == 1) 
     // décaler à droite puis comparer avec 1 (en utilisant AND) donne le bit à la position donnée
         res = BIT_1;
     else
