c503d5e55595b2459839f94cf45827a6de3e4c29
fzhou <fangxuan.zhou@insa-rouen.fr>
2023-12-29 12:46:22 +0100

    I tried to do buildCodingTable

--- programme/src/compress.c
+++ programme/src/compress.c
@@ -1,7 +1,7 @@
 #include "compress.h"
 #include "statistics.h"
 #include "huffmanTree.h"
-
+#include "codingTable.h"
 
 bool compress(FILE *input, FILE *output) {
     // TODO
@@ -21,3 +21,18 @@ Statistics compute_statistics(FILE *input){
     }
     return stat;
 }
+
+//La fonction huffman_tree_from_statistic correspond à buildHuffmanTree dans le rapport
+//Mais il me semble qu'elle n'est pas encore finie dans huffmanTree.c
+
+CodingTable coding_table_from_huffman_tree(HuffmanTree tree){
+    CodingTable table;
+    BinaryCode code;
+    Byte byte = 0;
+
+    table = coding_table_create();
+    code = binary_code_create();
+    coding_table_from_huffman_tree_recursive(tree, &table, &code, byte);
+    return table;
+}
+// Pour être franc je comprends pas pourquoi on utilise la variable byte ici.
