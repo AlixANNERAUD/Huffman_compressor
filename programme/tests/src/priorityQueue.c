44a8243ad9715cf4e1aca3293a3dd0872d7e78c5
Alix ANNERAUD <alix@anneraud.fr>
2023-12-29 17:18:05 +0100

    Add a unit test for priority queue  #40

--- programme/tests/src/priorityQueue.c
+++ programme/tests/src/priorityQueue.c
@@ -18,24 +18,48 @@ void test_priority_queue_get_length() {
     priority_queue_push(&pq, a);
     priority_queue_push(&pq, b);
     CU_ASSERT_EQUAL(priority_queue_get_length(&pq), 2);
-    priority_queue_pop(&pq);
+    huffman_tree_delete(priority_queue_pop((&pq)));
     CU_ASSERT_EQUAL(priority_queue_get_length(&pq), 1);
-
+    huffman_tree_delete(priority_queue_pop((&pq)));
+    CU_ASSERT_EQUAL(priority_queue_get_length(&pq), 0);
 }
 
 void test_priority_queue_push_and_pop() {
-
-    PriorityQueue pq;
-    pq = priority_queue_create();
     HuffmanTree a = huffman_tree_new_leaf('a', 2);
-    int length_before = pq.length;
+    PriorityQueue pq = priority_queue_create();
     priority_queue_push(&pq, a);
-    CU_ASSERT_EQUAL(priority_queue_get_length(&pq)-length_before, 1);
-    length_before = pq.length;
+    CU_ASSERT_EQUAL(priority_queue_get_length(&pq), 1);
     HuffmanTree ht = priority_queue_pop(&pq);
     CU_ASSERT_EQUAL(ht, a);
-    CU_ASSERT_EQUAL(priority_queue_get_length(&pq)-length_before, -1);
+    CU_ASSERT_EQUAL(priority_queue_get_length(&pq), 0);
+    huffman_tree_delete(ht);
 
+}
+
+void test_priority_queue_sort() {
+    PriorityQueue pq = priority_queue_create();
+    for (int i = 0; i < 100; i++) {
+        HuffmanTree ht = huffman_tree_new_leaf('a', i);
+        priority_queue_push(&pq, ht);   // Ajout de nœud avec un poids croissant pour forcer le triage
+    }
+    CU_ASSERT_EQUAL(priority_queue_get_length(&pq), 100);
+    for (int i = 0; i < 100; i++) {
+        HuffmanTree ht = priority_queue_pop(&pq);
+        CU_ASSERT_EQUAL(huffman_tree_get_weight(ht), i);    // Vérification le défilement renvoie les nœuds dans l'ordre croissant
+        huffman_tree_delete(ht);
+    }
+    CU_ASSERT_EQUAL(priority_queue_get_length(&pq), 0);
+    for (int i = 0; i < 100; i++) {
+        HuffmanTree ht = huffman_tree_new_leaf(i, 0xABCD);
+        priority_queue_push(&pq, ht);   // Ajout de nœud avec une valeur croissante pour forcer le triage
+    }
+    CU_ASSERT_EQUAL(priority_queue_get_length(&pq), 100);
+    for (int i = 0; i < 100; i++) {
+        HuffmanTree ht = priority_queue_pop(&pq);
+        CU_ASSERT_EQUAL(huffman_tree_get_value(ht), i);    // Vérification le défilement renvoie les nœuds dans l'ordre croissant
+        huffman_tree_delete(ht);
+    }
+    CU_ASSERT_EQUAL(priority_queue_get_length(&pq), 0);
 }
 
 void priority_queue_add_tests()
@@ -44,4 +68,5 @@ void priority_queue_add_tests()
     CU_add_test(suite, "test_priority_queue_create", test_priority_queue_create);
     CU_add_test(suite, "test_priority_queue_get_length", test_priority_queue_get_length);
     CU_add_test(suite, "test_priority_queue_push_and_pop", test_priority_queue_push_and_pop);
+    CU_add_test(suite, "test_priority_queue_sort", test_priority_queue_sort);
 }
