26fe2e2c7a9a9f76a517b1fb0c77ed1fe5ec4de1
fzhou <fangxuan.zhou@insa-rouen.fr>
2023-12-29 12:08:34 +0100

    I tried to do compute_statistics

--- programme/src/compress.c
+++ programme/src/compress.c
@@ -1,7 +1,24 @@
 #include "compress.h"
+#include "statistics.h"
+#include "byte.h"
+
+
 
 bool compress(FILE *input, FILE *output) {
     // TODO
     return false;
 }
 
+Statistics compute_statistics(FILE *input){
+    Byte byte;
+    Statistics stat;
+    
+    stat = statistics_create();
+    fseek(input,0,SEEK_SET); 
+    while (feof(input) == 0)
+    {
+        fread(&byte,sizeof(byte),1,input);
+        statistics_increase_count(&stat, byte);
+    }
+    return stat;
+}
