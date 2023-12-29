5e2940d6de2b9ab53547da86e994956005a87dc6
Alix ANNERAUD <alix@anneraud.fr>
2023-12-29 17:19:49 +0100

    Format and fix priority queue push function and a small fix #34

--- programme/lib/src/priorityQueue.c
+++ programme/lib/src/priorityQueue.c
@@ -1,41 +1,45 @@
 #include "priorityQueue.h"
 
-PriorityQueue priority_queue_create() {
+PriorityQueue priority_queue_create()
+{
     PriorityQueue priorityQueue;
     priorityQueue.length = 0;
     return priorityQueue;
 }
 
-size_t priority_queue_get_length(const PriorityQueue *priorityQueue) {
+size_t priority_queue_get_length(const PriorityQueue *priorityQueue)
+{
     // Préconditions
     assert(priorityQueue != NULL);
 
     return priorityQueue->length;
 }
 
-HuffmanTree priority_queue_pop(PriorityQueue *priorityQueue) {
+HuffmanTree priority_queue_pop(PriorityQueue *priorityQueue)
+{
     // Préconditions
     assert(priorityQueue != NULL);
     assert(priorityQueue->length > 0);
 
-    HuffmanTree node = priorityQueue->nodes[priorityQueue->length - 1];
-    priorityQueue->length--;
+    priorityQueue->length--; 
+    HuffmanTree node = priorityQueue->nodes[priorityQueue->length];
     return node;
 }
 
-void priority_queue_push(PriorityQueue *priorityQueue, HuffmanTree node) {
+void priority_queue_push(PriorityQueue *priorityQueue, HuffmanTree node)
+{
     // Préconditions
-    assert(priorityQueue != NULL);
-    assert(priorityQueue->length < PRIORITY_QUEUE_MAXIMUM_SIZE);
+    assert(priorityQueue); // Pointeur non nul
+    assert(priorityQueue->length < PRIORITY_QUEUE_MAXIMUM_SIZE); // File non pleine
 
-    size_t i = priorityQueue->length;
+    int i = priorityQueue->length; // On commence par la fin car on devra probablement décaler les éléments vers la fin de la file.
     // On décale les éléments de la file pour insérer le nouvel élément
-    // TODO : à revoir pour adapter au type arbre d'Huffman
-    while (priorityQueue->nodes[i - 1] > node && i > 0) {
+    while (huffman_tree_greater_than(node, priorityQueue->nodes[i - 1]) && i > 0)
+    {
         priorityQueue->nodes[i] = priorityQueue->nodes[i - 1];
         i--;
     }
-    
+    priorityQueue->nodes[i] = node;
+
     priorityQueue->length++;
 }
-
