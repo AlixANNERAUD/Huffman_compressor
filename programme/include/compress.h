07e2ba08a214e8c22f60a95f05d520325c53ffb6
atranouez <alienor.tranouez@insa-rouen.fr>
2023-12-30 20:31:16 +0100

    Add the right inclusions

--- programme/include/compress.h
+++ programme/include/compress.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #include "common.h"
+#include "compress.h"
+#include "statistics.h"
+#include "huffmanTree.h"
+#include "codingTable.h"
+#include "binaryCode.h""
 #include <stdio.h>
 
 bool compress(FILE *in, FILE *out);
