28aac19ac45f5b0761fcabaae777236e696ea93b
Alix ANNERAUD <alix@anneraud.fr>
2024-01-04 17:19:24 +0100

    Write sample main

--- programme/src/main.c
+++ programme/src/main.c
@@ -1,6 +1,18 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
 
+int main(int argc, char *argv[]) {
 
-int main() {
-    printf("Hello world !");
+    for (int i = 0; i < argc; i++) {
+        printf("Argument %d : %s\n", i, argv[i]);
+    }
+
+    if (getcwd(NULL, 0) != NULL) {
+        printf("Répertoire courant : %s\n", getcwd(NULL, 0));
+    } else {
+        printf("Erreur lors de la récupération du répertoire courant\n");
+    }
+
+    return EXIT_SUCCESS;
 }
