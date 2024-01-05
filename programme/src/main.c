2fe12226520acf776a3d0ad553feb8a36c4c84aa
Alix ANNERAUD <alix@anneraud.fr>
2024-01-05 18:15:45 +0100

    Refactor main with functions

--- programme/src/main.c
+++ programme/src/main.c
@@ -1,60 +1,167 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "unistd.h"
-#include "string.h"
-#include <compress.h>
-#include <decompress.h>
+// - Dépendances
+// - - Librairie standard
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+// - - Librairie système
+#include <unistd.h>
+// - - Local
+#include "compress.h"
+#include "decompress.h"
 
-int main(int argc, char *argv[]) {
+void print_error(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+}
 
-    for (int i = 0; i < argc; i++) {
+/// @brief Fonction qui vérifie si une chaîne de caractères se termine par une autre
+/// @param string Chaîne de caractères à vérifier
+/// @param suffix Chaîne de caractères à chercher
+/// @return `true` si `string` se termine par `suffix`, `false` sinon
+bool ends_with(const char *string, const char *suffix)
+{
+    size_t strLength = strlen(string);
+    size_t suffixLength = strlen(suffix);
+    if (strLength < suffixLength)
+        return false;
+    else
+        return strncmp(string + strLength - suffixLength, suffix, suffixLength) == 0;
+}
+
+/// @brief Fonction qui lit les arguments passés au programme
+/// @param argc Nombre d'arguments
+/// @param argv Arguments
+/// @param mode Résultat de la lecture du mode
+/// @param intput_file_path Résultat de la lecture du chemin du fichier d'entrée
+/// @return `true` si les arguments ont bien été lus, `false` sinon
+bool read_arguments(int argc, const char *argv[], char *mode, char **intput_file_path)
+{
+    // - On affiche les arguments
+    for (int i = 0; i < argc; i++)
         printf("Argument %d : %s\n", i, argv[i]);
-    }
 
-    if (argc != 3) {
-        printf("Mauvais nombre d'arguments renseigné\n");
-        return 1;
+    // - On vérifie le nombre d'arguments
+    if (argc != 3)
+    {
+        print_error("Invalid number of arguments. Expected 2, got %d\n", argc - 1);
+        return false;
     }
-    if (getcwd(NULL, 0) != NULL) {
-        printf("Répertoire courant : %s\n", getcwd(NULL, 0));
-    } else {
-        printf("Erreur lors de la récupération du répertoire courant\n");
-        return 1;
+    // - On récupère et vérifie la commande
+    *mode = *argv[1];
+
+    if (*mode != 'c' && *mode != 'd')
+    {
+        print_error("Invalid compression mode. Expected c or d, got %s\n", mode);
+        return false;
     }
+    // - On récupère le nom du fichier d'entrée
+    *intput_file_path = argv[2];
 
-    char * mode = argv[1];
-    char * filename = argv[2];
+    return true;
+}
 
-    FILE * input;
-    FILE * output; 
+/// @brief Fonction qui ouvre les fichiers en fonction du mode
+/// @param mode Mode
+/// @param input_file Fichier d'entrée résultant
+/// @param output_file Fichier de sortie résultant
+/// @return `true` si les fichiers ont bien été ouverts, `false` sinon
+bool open_files(char mode, const char *input_file_path, FILE **input_file, FILE **output_file)
+{
+    char *output_file_path = malloc(strlen(input_file_path) + 6); // +6 pour le ".huff\0" (pas forcément nécessaire dans le cas de la décompression, mais plus simple)
+    if (mode == 'c')
+    {
+        // - Création du nom du fichier de sortie
+        strcpy(output_file_path, input_file_path); // On copie le nom du fichier d'entrée
+        strcat(output_file_path, ".huff");         // On ajoute l'extension
 
-    if (*mode == 'c'){
-        CompressResult cr;
-        input = fopen(filename, "rb");
-        output = fopen(filename, "wb");
-        cr = compress(input, output);
-        /*
-        char buffer[40];
-        FileSize buffer_size = sizeof(buffer);
-        compress_error_to_string(cr, buffer, buffer_size);
-        printf("%s", buffer);
-        */
-        fclose(input);
-        fclose(output);
-    } else if (*mode == 'd'){
-        DecompressResult dr;
-        input = fopen(filename, "rb");
-        output = fopen(filename, "wb");
-        dr = decompress(input, output);
-        char buffer[40];
-        FileSize buffer_size = sizeof(buffer);
-        decompress_error_to_string(dr, buffer, buffer_size);
-        printf("%s", buffer);
-        fclose(input);
-        fclose(output);
-    } else {
-        printf("Le deuxième argument doit être c (compresser) ou d (décompresser). %s est un argument invalide.", mode);
-        return 1;
+        // - On ouvre les fichiers
+        *input_file = fopen(input_file_path, "r");
+        *output_file = fopen(output_file_path, "w");
     }
+    else if (mode == 'd')
+    {
+        // - On vérifie que le fichier d'entrée est bien un fichier .huff
+        if (!ends_with(input_file_path, ".huff"))
+        {
+            print_error("Invalid input file. Expected a .huff file, got %s\n", input_file_path);
+            return false;
+        }
+        // - Création du nom du fichier de sortie
+        strncpy(output_file_path, input_file_path, strlen(input_file_path) - 5); // On enlève les 5 derniers caractères (".huff")
+        // - On ouvre les fichiers
+        *input_file = fopen(input_file_path, "r");
+        *output_file = fopen(output_file_path, "w"); // On ouvre le fichier de sortie
+    }
+    free(output_file_path); // On libère la mémoire allouée pour le nom du fichier de sortie
+    rewind(*input_file);    // On remet le curseur au début du fichier d'entrée
+    // - On vérifie que les fichiers ont bien été ouverts
+    if (*input_file == NULL)
+    {
+        print_error("Error while opening input file.\n");
+        return false;
+    }
+    if (*output_file == NULL)
+    {
+        print_error("Error while opening output file.\n");
+        return false;
+    }
+    return true;
+}
+
+int main(int argc, const char *argv[])
+{
+    char mode = '\0';
+    char *input_file_path;
+
+    if (!read_arguments(argc, argv, &mode, &input_file_path))
+        return EXIT_FAILURE;
+
+    printf("Current working directory : %s\n", getcwd(NULL, 0)); // "getcwd" = "get current working directory
+    printf("Input file path : %s\n", input_file_path);
+
+    FILE *inputFile;
+    FILE *outputFile;
+
+    if (!open_files(mode, input_file_path, &inputFile, &outputFile))
+        return EXIT_FAILURE;
+
+    
+
+    if (mode == 'c')
+    {
+        printf("Compressing ...\n");
+        CompressResult compressResult = compress(inputFile, outputFile);
+        if (compressResult != COMPRESS_RESULT_OK)
+        {
+            printf("Error while compressing file.\n");
+            // char errorBuffer[1024];
+            // FileSize buffer_size = sizeof(errorBuffer);
+            // compress_error_to_string(cr, errorBuffer, buffer_size);
+            // print_error("%s", errorBuffer);
+        }
+        else
+            printf("File compressed successfully.\n");
+    }
+    else if (mode == 'd')
+    {
+        printf("Decompressing ...\n");
+        DecompressResult decompressResult = decompress(inputFile, outputFile);
+        if (decompressResult != DECOMPRESS_RESULT_OK)
+        {
+            char errorBuffer[1024];
+            FileSize buffer_size = sizeof(errorBuffer);
+            decompress_error_to_string(decompressResult, errorBuffer, buffer_size);
+            print_error("%s", errorBuffer);
+        }
+        else
+            printf("File decompressed successfully.\n");
+    }
+ 
+    fclose(inputFile);
+    fclose(outputFile);
     return EXIT_SUCCESS;
 }
