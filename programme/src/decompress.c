3eb6b387a64a415f9d321d0e59db904c68ad7a07
Alix ANNERAUD <alix@anneraud.fr>
2024-01-07 00:41:06 +0100

    Fix bug when decompressing the last bit (use original file size instead of waiting current file end)

--- programme/src/decompress.c
+++ programme/src/decompress.c
@@ -37,40 +37,45 @@ DecompressResult read_header(FILE *input, Statistics statistics)
     return DECOMPRESS_RESULT_OK;
 }
 
-DecompressResult decompress_data(FILE *input, FILE *output, const HuffmanTree tree)
+DecompressResult decompress_data(FILE *input, FILE *output, const HuffmanTree tree, FileSize fileSize)
 {
     unsigned char bitReadPosition = 8;
     HuffmanTree currentTree = tree;
     Byte sourceByte = byte_create(0);
 
-    while (feof(input) == 0)
+    FileSize decompressedBytes = 0;
+
+    while (decompressedBytes < fileSize)
     {
-        // BinaryCode code = binary_code_create();
+        // - Lecture d'un octet du fichier d'entrée si on a lu tous les bits de l'octet précédent
         if (bitReadPosition > 7)
         {
             unsigned char bitsRead;
             if (fread(&bitsRead, sizeof(bitsRead), 1, input) != 1)
-                break;
+                return DECOMPRESS_RESULT_ERROR_PREMATURE_END_OF_FILE;
             sourceByte = byte_create(bitsRead); // Conversion d'un naturel non signé d'un octet en octet
             bitReadPosition = 0;
         }
 
-        if (!huffman_tree_is_leaf(currentTree))
-        {                                                        // Si c'est une feuille
+        // - Si c'est une feuille, on écrit la valeur de l'octet dans le fichier de sortie
+        if (huffman_tree_is_leaf(currentTree))
+        {                                                                        // Si c'est une feuille
+            Byte destinationByte = huffman_tree_get_value(currentTree);          // On récupère la valeur de l'octet
+            unsigned char naturalToWrite = byte_to_natural(destinationByte);     // Conversion d'un octet en naturel non signé d'un octet
+            if (fwrite(&naturalToWrite, sizeof(naturalToWrite), 1, output) != 1) // Ecriture de l'octet dans le fichier de sortie
+                return DECOMPRESS_RESULT_ERROR_FAILED_TO_WRITE_OUTPUT_FILE;
+            decompressedBytes++;
+            currentTree = tree; // Retour à la racine
+        }
+        // - Sinon, on continue de parcourir l'arbre
+        else
+        {
             Bit bit = byte_get_bit(sourceByte, bitReadPosition); // On récupère le bit
             if (bit == 0)
                 currentTree = currentTree->leftChild;
             else
                 currentTree = currentTree->rightChild;
             bitReadPosition++;
-        }
-        else
-        {
-            Byte destinationByte = huffman_tree_get_value(currentTree);          // On récupère la valeur de l'octet
-            unsigned char naturalToWrite = byte_to_natural(destinationByte);     // Conversion d'un octet en naturel non signé d'un octet
-            if (fwrite(&naturalToWrite, sizeof(naturalToWrite), 1, output) != 1) // Ecriture de l'octet dans le fichier de sortie
-                return DECOMPRESS_RESULT_ERROR_FAILED_TO_WRITE_OUTPUT_FILE;
-            currentTree = tree; // Retour à la racine
         }
     }
 
@@ -91,7 +96,9 @@ DecompressResult decompress(FILE *input, FILE *output)
 
     HuffmanTree tree = huffman_tree_from_statistic(statistics);
 
-    result = decompress_data(input, output, tree);
+    printf("TOtal count : %lu\n", statistics_get_total_count(statistics));
+
+    result = decompress_data(input, output, tree, statistics_get_total_count(statistics));
 
     huffman_tree_delete(tree);
 
