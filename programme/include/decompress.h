c49f5c54e223d48218683562a160f975d9071314
Alix ANNERAUD <alix@anneraud.fr>
2024-01-07 00:09:37 +0100

    Fix decompress_error_to_string signature

--- programme/include/decompress.h
+++ programme/include/decompress.h
@@ -15,6 +15,7 @@ typedef enum {
     DECOMPRESS_RESULT_ERROR_PREMATURE_END_OF_FILE,
     DECOMPRESS_RESULT_ERROR_INVALID_HEADER,
     DECOMPRESS_RESULT_ERROR_FAILED_TO_WRITE_OUTPUT_FILE,
+    DECOMPRESS_RESULT_FAILED_TO_CHECK_OUTPUT_FILE,
     DECOMPRESS_RESULT_INCONSISTENT_DECOMPRESSED_FILE
 } DecompressResult;
 
@@ -26,5 +27,5 @@ typedef enum {
 /// @return Résultat de la décompression
 DecompressResult decompress(FILE* input, FILE* output);
 
-DecompressResult decompress_error_to_string(DecompressResult error, char* buffer, FileSize buffer_size);
+DecompressResult decompress_error_to_string(DecompressResult error, char* buffer, size_t buffer_size);
 
