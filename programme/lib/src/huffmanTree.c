3d621587d8080a1b99c64f86675925028247e762
Alix ANNERAUD <alix@anneraud.fr>
2024-01-06 23:48:15 +0100

    Reformat and fix bug when tree is created

--- programme/lib/src/huffmanTree.c
+++ programme/lib/src/huffmanTree.c
@@ -31,7 +31,7 @@ HuffmanTree huffman_tree_new_tree(HuffmanTree left, HuffmanTree right)
     assert(left);
     assert(right);
 
-    HuffmanTree tree = huffman_tree_new_leaf(byte_create(0), 0);
+    HuffmanTree tree = huffman_tree_new_leaf(byte_create(0), huffman_tree_get_weight(left) + huffman_tree_get_weight(right));
     tree->leftChild = left;
     tree->rightChild = right;
 
@@ -58,7 +58,7 @@ void huffman_tree_delete(HuffmanTree tree)
 
 bool huffman_tree_is_leaf(const HuffmanTree tree)
 {
-    // Précondtion
+    // Préconditions
     assert(tree);
 
     return ((tree->leftChild == NULL) && (tree->rightChild == NULL));
@@ -75,7 +75,7 @@ Byte huffman_tree_get_value(const HuffmanTree tree)
 
 unsigned int huffman_tree_get_weight(const HuffmanTree tree)
 {
-    // Précondtion
+    // Préconditions
     assert(tree);
 
     return tree->weight;
@@ -99,7 +99,7 @@ HuffmanTree huffman_tree_get_right_child(const HuffmanTree tree)
     return tree->rightChild;
 }
 
-HuffmanTree *huffman_tree_from_statistic(const Statistics *stat)
+HuffmanTree huffman_tree_from_statistic(const Statistics stat)
 {
     // Précondition
     assert(stat);
@@ -108,27 +108,33 @@ HuffmanTree *huffman_tree_from_statistic(const Statistics *stat)
     HuffmanTree leaf;
     HuffmanTree rightLeaf;
     HuffmanTree leftLeaf;
-    Byte byte;
     FileSize count;
     PriorityQueue queue = priority_queue_create();
 
-    for (FileSize i=0;i<256;i++){
-        count = stat[i];
-        if(count>0){
+    for (unsigned int i = 0; i < 256; i++)
+    {
+        count = statistics_get_count(stat, byte_create(i));
+        if (count > 0)
+        {
             leaf = huffman_tree_new_leaf(byte_create(i), count);
             priority_queue_push(&queue, leaf);
         }
     }
-    while ((priority_queue_get_length)>0){
+
+    while (priority_queue_get_length(&queue) > 1)
+    {
         leftLeaf = priority_queue_pop(&queue);
-        if ((priority_queue_get_length)>0){
+        if (priority_queue_get_length(&queue) > 0)
+        {
             rightLeaf = priority_queue_pop(&queue);
             tree = huffman_tree_new_tree(leftLeaf, rightLeaf);
         }
-        else {
+        else
             tree = leftLeaf;
-        }
+
+        priority_queue_push(&queue, tree);
     }
+
     return tree;
 }
 
@@ -138,7 +144,7 @@ bool huffman_tree_greater_than(HuffmanTree tree1, HuffmanTree tree2)
     assert(tree1);
     assert(tree2);
 
-    if (huffman_tree_get_weight(tree1) > huffman_tree_get_weight(tree2))    // Si le poids de tree1 est plus grand que celui de tree2, on retourne vrai
+    if (huffman_tree_get_weight(tree1) > huffman_tree_get_weight(tree2)) // Si le poids de tree1 est plus grand que celui de tree2, on retourne vrai
         return true;
 
     if (huffman_tree_get_weight(tree1) == huffman_tree_get_weight(tree2) && huffman_tree_is_leaf(tree1) && huffman_tree_is_leaf(tree2)) // Si les poids sont égaux et que les deux arbres sont des feuilles, on compare les valeurs
