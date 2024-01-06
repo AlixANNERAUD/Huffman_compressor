4b98b516e16d4c137ff5ef837a1f9fab9fbb59d7
Alix ANNERAUD <alix@anneraud.fr>
2024-01-07 00:08:59 +0100

    Update and add unit tests for codingTable

--- programme/tests/src/codingTable.c
+++ programme/tests/src/codingTable.c
@@ -4,48 +4,56 @@
 
 #define CODING_TABLE_MAXIMUM_SIZE 256
 
-void test_coding_table_create(){
+void test_coding_table_create()
+{
     CodingTable table = coding_table_create();
-    CU_ASSERT_EQUAL(table.length,0);
+    CU_ASSERT_EQUAL(table.length, 0);
 }
 
-void test_coding_table_add(){
+void test_coding_table_add()
+{
     CodingTable table = coding_table_create();
 
     BinaryCode bc = binary_code_create();
     binary_code_add_bit(&bc, BIT_0);
 
-    coding_table_add(&table,'a',bc);
-    CU_ASSERT_EQUAL(table.length,1);
-    CU_ASSERT_EQUAL(table.entries[0].key,'a');
-    CU_ASSERT_EQUAL(table.entries[0].value.bits[0],bc.bits[0]);
+    coding_table_add(&table, byte_create('a'), bc);
+    CU_ASSERT_EQUAL(table.length, 1);
+    CU_ASSERT_EQUAL(table.entries[0].key, byte_create('a'));
+    CU_ASSERT_EQUAL(table.entries[0].value.bits[0], bc.bits[0]);
 
     binary_code_add_bit(&bc, BIT_1);
 
-    coding_table_add(&table,'b',binary_code_create());
-    coding_table_add(&table,'c',binary_code_create());
-    CU_ASSERT_EQUAL(table.length,3);
+    coding_table_add(&table, byte_create('b'), binary_code_create());
+    coding_table_add(&table, byte_create('c'), binary_code_create());
+    CU_ASSERT_EQUAL(table.length, 3);
 
-    CU_ASSERT_EQUAL(table.entries[2].key,'c');
+    CU_ASSERT_EQUAL(table.entries[2].key, byte_create('c'));
 }
 
-void test_coding_table_get_value(){
+void test_coding_table_get_value()
+{
     CodingTable table = coding_table_create();
-    coding_table_add(&table,'a',binary_code_create());
-    CU_ASSERT(coding_table_is_present(&table,'a'));
-    coding_table_add(&table,'b',binary_code_create());
-    CU_ASSERT(coding_table_is_present(&table,'b'));
-    CU_ASSERT(!coding_table_is_present(&table,'c'));
+    coding_table_add(&table, byte_create('a'), binary_code_create());
+    CU_ASSERT(coding_table_search(&table, byte_create('a'), NULL));
+    coding_table_add(&table, byte_create('b'), binary_code_create());
+    CU_ASSERT(coding_table_search(&table, byte_create('b'), NULL));
+    CU_ASSERT(!coding_table_search(&table, byte_create('c'), NULL));
 }
 
-void test_coding_table_is_present(){
+void test_coding_table_search()
+{
     CodingTable table = coding_table_create();
-    coding_table_add(&table,'a',binary_code_create());
-    coding_table_add(&table,'b',binary_code_create());
-    coding_table_add(&table,'c',binary_code_create());
-    CU_ASSERT_TRUE(coding_table_is_present(&table,'a'));
-    CU_ASSERT_TRUE(coding_table_is_present(&table,'b'));
-    CU_ASSERT_TRUE(coding_table_is_present(&table,'c'));
+    coding_table_add(&table, byte_create('a'), binary_code_create());
+    coding_table_add(&table, byte_create('b'), binary_code_create());
+    coding_table_add(&table, byte_create('c'), binary_code_create());
+    size_t index;
+    CU_ASSERT(coding_table_search(&table, byte_create('a'), &index));
+    CU_ASSERT_EQUAL(index, 0);
+    CU_ASSERT(coding_table_search(&table, byte_create('b'), NULL));
+    CU_ASSERT(coding_table_search(&table, byte_create('c'), &index));
+    CU_ASSERT_EQUAL(index, 2);
+    CU_ASSERT(!coding_table_search(&table, byte_create('d'), NULL));
 }
 
 void coding_table_add_tests()
@@ -54,5 +62,5 @@ void coding_table_add_tests()
     CU_add_test(suite, "test_coding_table_create", test_coding_table_create);
     CU_add_test(suite, "test_coding_table_add", test_coding_table_add);
     CU_add_test(suite, "test_coding_table_get_value", test_coding_table_get_value);
-    CU_add_test(suite, "test_coding_table_is_present", test_coding_table_is_present);
+    CU_add_test(suite, "test_coding_table_is_present", test_coding_table_search);
 }
