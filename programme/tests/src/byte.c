a89bb5a08ccfae480a50a275f097df46e9284a53
Alix ANNERAUD <alix@anneraud.fr>
2024-01-06 23:43:17 +0100

    Add further testing to byte

--- programme/tests/src/byte.c
+++ programme/tests/src/byte.c
@@ -1,32 +1,68 @@
 #include "byte.h"
 #include <CUnit/Basic.h>
 
-void test_byte_create(){
-    Byte a = byte_create(0);
-    Byte b = byte_create(255);    
-    Byte c = byte_create(122);
-    CU_ASSERT_EQUAL(a,0);
-    CU_ASSERT_EQUAL(b,255);
-    CU_ASSERT_EQUAL(c,122);
+void test_byte_create()
+{
+    for (unsigned int i = 0; i < 256; i++)
+    {
+        Byte b = byte_create(i);
+        CU_ASSERT_EQUAL(b, i);
+    }
 }
 
-void test_byte_to_natural(){
+void test_byte_to_natural()
+{
     Byte b = byte_create(122);
-    CU_ASSERT_EQUAL(b,122);
+    CU_ASSERT_EQUAL(b, 122);
 }
 
-void test_byte_set_bit(){
+void test_byte_set_bit()
+{
     Byte b = byte_create(128);
-    byte_set_bit(&b,7,BIT_0);
-    CU_ASSERT_EQUAL(b,0);
-    byte_set_bit(&b,0,BIT_1);
-    CU_ASSERT_EQUAL(b,1);
+    byte_set_bit(&b, 7, BIT_0);
+    CU_ASSERT_EQUAL(b, 0);
+    byte_set_bit(&b, 0, BIT_1);
+    CU_ASSERT_EQUAL(b, 1);
 }
 
-void test_byte_get_bit(){
+void test_byte_get_bit()
+{
     Byte b = byte_create(128);
-    Bit b0 = byte_get_bit(b,7);
-    CU_ASSERT_EQUAL(b0,BIT_1);
+    Bit b0 = byte_get_bit(b, 7);
+    CU_ASSERT_EQUAL(b0, BIT_1);
+}
+
+void test_byte_set_and_get_bit()
+{
+    for (unsigned int i = 0; i < 8; i++)
+    {
+        Byte b = byte_create(0);
+        byte_set_bit(&b, i, BIT_0);
+        CU_ASSERT_EQUAL(byte_get_bit(b, i), BIT_0);
+        byte_set_bit(&b, i, BIT_1);
+        for (unsigned int j = 0; j < i; j++)
+        {
+            if (j == i)
+                CU_ASSERT_EQUAL(byte_get_bit(b, j), BIT_1)
+            else
+                CU_ASSERT_EQUAL(byte_get_bit(b, j), BIT_0);
+        }
+    }
+
+    for (unsigned int i = 0; i < 8; i++)
+    {
+        Byte b = byte_create(255);
+        byte_set_bit(&b, i, BIT_1);
+        CU_ASSERT_EQUAL(byte_get_bit(b, i), BIT_1);
+        byte_set_bit(&b, i, BIT_0);
+        for (unsigned int j = 0; j < i; j++)
+        {
+            if (j == i)
+                CU_ASSERT_EQUAL(byte_get_bit(b, j), BIT_0)
+            else
+                CU_ASSERT_EQUAL(byte_get_bit(b, j), BIT_1);
+        }
+    }
 }
 
 void byte_add_tests()
@@ -36,4 +72,5 @@ void byte_add_tests()
     CU_add_test(suite, "test_byte_to_natural", test_byte_to_natural);
     CU_add_test(suite, "test_byte_set_bit", test_byte_set_bit);
     CU_add_test(suite, "test_byte_get_bit", test_byte_get_bit);
-} 
+    CU_add_test(suite, "test_byte_set_and_get_bit", test_byte_set_and_get_bit);
+}
