5da6bdd2a0e187beb727ed681a85e5caa9efde2e
Alix ANNERAUD <alix@anneraud.fr>
2024-01-09 19:59:10 +0100

    Rewrite functional test

--- programme/tests/functional_test.py
+++ programme/tests/functional_test.py
@@ -1,9 +1,33 @@
+import random
 
+filesSize = 10_000_000
 
-file = open("test", "wb")
+# - Fichier vide
+emptyFile = open("empty", "wb")
+emptyFile.close()
 
-for i in range(0, 10_000_000):
-    file.write(bytes([i % (0x50)]))
-
+# - Fichier avec un unique caractère
+file = open("unique", "wb")
+for i in range(filesSize):
+    file.write(bytes([1]))
 file.close()
+
+# - Fichier aléatoire
+randomFile = open("random", "wb")
+for i in range(filesSize):
+    randomFile.write(random.randbytes(1))
+randomFile.close()
+
+# - Fichier équilibré
+balancedFile = open("balanced", "wb")
+for i in range(filesSize):
+    balancedFile.write(bytes([i % (0xFF + 1)]))
+balancedFile.close()
+
+# - Fichier déséquilibré
+unbalancedFile = open("test", "wb")
+for i in range(filesSize / 256):
+    for j in range(i % (0xFF + 1) + 1):
+        unbalancedFile.write(bytes([i % (0xFF + 1)]))
+unbalancedFile.close()
 
